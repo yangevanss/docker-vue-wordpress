9ee5af2f8847a1ca5c09bc1e2d945c6a59b27319
Evan <evan@blockstudio.tw>
2021-06-08 14:17:43 +0800

    [data] 條細節

--- wordpress/wp-content/themes/blockstudio-theme/archive-news.php
+++ wordpress/wp-content/themes/blockstudio-theme/archive-news.php
@@ -2,38 +2,34 @@
 
 $post_type = get_post_type();
 
-if($post_type){
+if ($post_type) {
     $context = Timber::context();
-    $context['category_terms'] = get_terms([
-        'taxonomy' => [
-            'category',
-            $post_type . '_category',
-        ],
-        'hide_empty' => false,
-    ]);
-    $context['tag_terms'] = get_terms([
-        'taxonomy' => [
-            'post_tag',
-            $post_type . '_tag',
-        ],
-        'hide_empty' => false,
-    ]);
-    
-    if(is_tax()){
+
+    $taxonomies = array_reduce(get_object_taxonomies($post_type), function ($acc, $taxonomy) {
+        $acc[$taxonomy] = get_terms([
+            'taxonomy' => [
+                $taxonomy,
+            ],
+            'hide_empty' => false,
+        ]);
+        return $acc;
+    }, []);
+
+    if (is_tax()) {
         /**
-         * ?[post_type]_['tag'|'category']=slug 
-         * /[post_type]_['tag'|'category']/slug 
+         * ?[taxonomy_name]=slug 
+         * /[taxonomy_name]/slug 
          */
     }
 
-    if(is_tag()){
+    if (is_tag()) {
         /** ?tag=slug */
     }
 
-    if(is_category()){
+    if (is_category()) {
         /** ?cat=cat_ID */
     }
-    
+
     Timber::render(array('pages/archive-' . $post_type . '.twig'), $context);
     return;
 }
